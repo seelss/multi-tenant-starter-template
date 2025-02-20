e820266e60dba3b62b6a7392c00a8fc49cf939ef
Zai Shi <zaishi00@outlook.com>
2025-02-20 12:30:12 -0800

    added logs

--- app/handler/[...stack]/page.tsx
+++ app/handler/[...stack]/page.tsx
@@ -2,5 +2,6 @@ import { StackHandler } from "@stackframe/stack";
 import { stackServerApp } from "../../../stack";
 
 export default function Handler(props: any) {
+  console.log(props.params, props.searchParams);
   return <StackHandler fullPage app={stackServerApp} {...props} />;
 }
